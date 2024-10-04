5f31a5f30c8037bb76d378bbb2b16d73a1819375
Pavel Masharov <p.masharov@pixelplex.io>
2024-10-04 11:55:00 +0200

    ts_ls added

--- lua/pamashar/plugins/lsp.lua
+++ lua/pamashar/plugins/lsp.lua
@@ -27,6 +27,7 @@ return {
         require("mason-lspconfig").setup({
             ensure_installed = {
                 "lua_ls",
+                "ts_ls",
             },
             handlers = {
                 function(server_name) -- default handler (optional)
