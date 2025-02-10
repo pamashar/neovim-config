8bc7eee109b2b3c10ecf758fdfd9282ccb494e2b
Pavel Masharov <p.n.masharov@gmail.com>
2025-02-10 16:42:18 +0100

    added git diff highlighting keymaps

--- lua/pamashar/plugins/gitsigns.lua
+++ lua/pamashar/plugins/gitsigns.lua
@@ -1,3 +1,14 @@
+vim.keymap.set(
+	'n',
+	'<leader>dh',
+    function ()
+        require('gitsigns').toggle_word_diff()
+        require('gitsigns').toggle_linehl()
+        require('gitsigns').toggle_deleted()
+    end,
+	{ desc = 'Toggle word diff' }
+)
+
 return {
     {
         "lewis6991/gitsigns.nvim",
