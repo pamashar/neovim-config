8c0da4f8f27b797893a4c788ecfb5c6291ab6b27
Pavel Masharov <p.n.masharov@gmail.com>
2025-02-10 16:43:24 +0100

    added git diffview files history keymaps

--- lua/pamashar/plugins/diffview.lua
+++ lua/pamashar/plugins/diffview.lua
@@ -1,4 +1,12 @@
-vim.keymap.set("n", "<leader>do", "<cmd>DiffviewOpen<CR>")
+vim.keymap.set("n", "<leader>do", "<cmd>DiffviewOpen<CR><C-w><C-w><cmd>:set number norelativenumber<CR><C-w><C-w><cmd>:set number norelativenumber<CR>")
+vim.keymap.set("n", "<leader>dho", "<cmd>DiffviewFileHistory<CR><C-w><C-w><cmd>:set number norelativenumber<CR><C-w><C-w><cmd>:set number norelativenumber<CR>")
+vim.keymap.set(
+    "n",
+    "<leader>dhf",
+    "<cmd>DiffviewFileHistory --follow %<CR><C-w><C-w><cmd>:set number norelativenumber<CR><C-w><C-w><cmd>:set number norelativenumber<CR>"
+)
+vim.keymap.set("n", "<leader>dhl", "<cmd>.DiffviewFileHistory --follow<CR><C-w><C-w><cmd>:set number norelativenumber<CR><C-w><C-w><cmd>:set number norelativenumber<CR>")
+vim.keymap.set("v", "<leader>dhb", "<cmd>'<,'>DiffviewFileHistory --follow<CR><C-w><C-w><cmd>:set number norelativenumber<CR><C-w><C-w><cmd>:set number norelativenumber<CR>")
 vim.keymap.set("n", "<leader>dc", "<cmd>DiffviewClose<CR>")
 
 return {
